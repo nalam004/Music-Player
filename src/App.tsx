e1170554ad8757226e63022b7ba1f4371b8ac4d6
nalam004 <niharikaalam1234@gmail.com>
2021-11-30 19:54:51 -0500

    updated ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import {musicList} from './data/music'
+import {musicList} from "./data/music"
 import { useState } from "react"
-import albumCover from './assets/album-cover.jpg'
+import albumCover from "./assets/1.jpg"
 
 function App() {
   
@@ -12,9 +12,10 @@ function App() {
   const bigPause = <img alt='' src="https://img.icons8.com/ios/256/F472B6/circled-pause--v1.png"/>
   const [song, setSong] = useState("Hello");
   const [artist, setArtist] = useState("Adele");
+  const [cover, setCover] = useState(albumCover);
   const [length, setLength] = useState("2:56");
 
-  function handleClick(index: number, song: string, artist: string, length: string) {
+  function handleClick(index: number, song: string, artist: string, cover: string, length: string) {
     // a boolean array with items initialized to false 
     let playing = isPlaying.map((val: boolean) => { return (val = false); }); 
     playing = playing.slice();
@@ -27,16 +28,8 @@ function App() {
     // sets states to current song playing
     setSong(song);
     setArtist(artist);
+    setCover(cover);
     setLength(length);
-  }
-  
-  function handleNext() {
-    // if a song is playing -- when isPlaying has a true value
-      // find the index
-      // set next index to true and previous index false
-      
-      let index = isPlaying.indexOf(true); 
-      
   }
 
   return (
@@ -48,7 +41,7 @@ function App() {
           <div id='music-player' className="items-center bg-white bg-opacity-10 flex flex-col w-full lg:w-auto lg:max-w-xs"
               style={{ backdropFilter: 'blur(5px)', }} >
 
-            <img src={albumCover} alt="album art" />
+            <img src={cover} alt="album art" />
             <div id="song-details" className="pt-3 text-center">
                 <h1 className="text-xl text-white font-bold tracking-wide">{song}</h1>
                 <p className="text-md text-gray-600 tracking-wide">{artist}</p>
@@ -56,11 +49,11 @@ function App() {
             
             <div id="progress" className="w-4/5">
                 <div id="progress-time" className="flex text-sm text-white justify-between pl-1 pr-1">
-                    <p className="start">1:30</p>
-                    <p className="end">{length}</p>
+                    <p id="start">1:20</p>
+                    <p id="end">{length}</p>
                 </div>
                 <div id="progress-bar" className="h-2 mt-3 bg-white rounded">
-                  <div id="inner-bar" className="w-1/2 h-full bg-pink-400 rounded rounded-r-none"></div>
+                  <div id="inner-bar" className="w-1/2 h-full bg-pink-400 rounded"></div>
                 </div>
             </div>
             
@@ -68,20 +61,21 @@ function App() {
               <button id="shuffle" className="w-6 m-2 transform hover:scale-110 motion-reduce:transform-none"><img alt='' src="https://img.icons8.com/ios/250/ffffff/shuffle.png"/></button>
               <button id="back" className="w-6 m-2 transform hover:scale-110 motion-reduce:transform-none"><img alt='' src="https://img.icons8.com/ios/250/ffffff/skip-to-start.png"/></button>
               <button id="play" className="w-10 m-2 transform hover:scale-110 motion-reduce:transform-none">{currPlaying ? bigPause : bigPlay}</button>
-              <button id="next" onClick={() => handleNext} className="w-6 m-2 transform hover:scale-110 motion-reduce:transform-none"><img alt='' src="https://img.icons8.com/ios/250/FFFFFF/end--v1.png"/></button>
+              <button id="next" className="w-6 m-2 transform hover:scale-110 motion-reduce:transform-none"><img alt='' src="https://img.icons8.com/ios/250/FFFFFF/end--v1.png"/></button>
               <button id="repeat" className="w-6 m-2 transform hover:scale-110 motion-reduce:transform-none"><img alt='' src="https://img.icons8.com/ios/250/ffffff/recurring-appointment.png"/></button>    
             </ul>
+            
           </div>
           
           <div id='playlist' className="flex-1 p-4 lg:p-6 overflow-y-auto overscroll-contain">
             {musicList.map((music, index) => (           
                 <div key={index} className='flex text-gray-700 border-b border-gray-800 items-center hover:bg-white hover:bg-opacity-25 hover:border-transparent'> 
-                    <img className='p-3 w-16 h-16 rounded-full' src={albumCover} alt='album-cover' />
+                    <img className='p-3 w-16 h-16 rounded-full' src={music.cover} alt='album-cover' />
                     <div className="p-3 w-full">{music.song}</div>
                     <div className="p-3 w-full">{music.artist}</div>
                     <div className="p-3 w-full">{music.album}</div>
                     <div className="p-3 w-full text-xs">{music.time}</div>
-                    <button id="play" className="m-3 w-36" onClick={() => handleClick(index, music.song, music.artist, music.time)}>
+                    <button id="play" className="m-3 w-36" onClick={() => handleClick(index, music.song, music.artist, music.cover, music.time)}>
                         { isPlaying[index] ? pause : play }
                     </button>              
                 </div>
--- src/data/music.js
+++ src/data/music.js
@@ -1,44 +1,59 @@
+import cover1 from '../assets/1.jpg';
+import cover2 from '../assets/2.jpg';
+import cover3 from '../assets/3.jpg';
+import cover4 from '../assets/4.jpg';
+import cover5 from '../assets/5.jpg';
+import cover6 from '../assets/6.jpg';
+import cover7 from '../assets/7.jpg';
+
 export const musicList = [
     {
-      song: 'hello',
-      artist: 'adele',
-      album: 'album',
-      time: '2:56',
+      song: 'Blood Bath',
+      artist: 'Delilah',
+      album: 'Red Waves',
+      cover: cover1,
+      time: '2:35',
     },
     {
-        song: 'rolling in the deep',
-        artist: 'adele',
-        album: 'album',
+        song: 'Orange Cream',
+        artist: 'Miranda',
+        album: 'Orangesickle',
+        cover: cover2,
         time: '3:56',
     },
     {
-        song: 'song',
-        artist: 'artist',
-        album: 'album',
+        song: 'Blues Clues',
+        artist: 'Oceania',
+        album: 'Geometric',
+        cover: cover3,
         time: '2:56',
     },
     {
-        song: 'song',
-        artist: 'artist',
-        album: 'album',
+        song: 'Lonely Nights',
+        artist: 'Amber',
+        album: 'Neon Lights',
+        cover: cover4,
+        time: '4:56',
+    },
+    {
+        song: 'Academic Pandemic',
+        artist: 'The Weekday',
+        album: 'Finals Week',
+        cover: cover5,
+        time: '3:06',
+    },
+    {
+        song: 'Cotton Candy',
+        artist: 'Doja Rat',
+        album: 'Lollipops',
+        cover: cover6,
         time: '2:56',
     },
     {
-        song: 'song',
-        artist: 'artist',
-        album: 'album',
-        time: '2:56',
-    },
-    {
-        song: 'song',
-        artist: 'artist',
-        album: 'album',
-        time: '2:56',
-    },
-    {
-        song: 'song',
-        artist: 'artist',
-        album: 'album',
-        time: '2:56',
+        song: 'Sand Dune',
+        artist: 'Mandrake',
+        album: 'Forest and Trees',
+        cover: cover7,
+        time: '4:12',
     }
 ]
